574fa45a17cc5133c75c9349b4f53be31e3b55fa
vince-platform <vincent@platform45.com>
2018-03-29 10:49:20 +0200

    Add user microposts with image support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
     #debugger
   end
 
@@ -53,15 +54,6 @@ class UsersController < ApplicationController
     end
 
     # Filters
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Confirms current user is correct user
     def correct_user
